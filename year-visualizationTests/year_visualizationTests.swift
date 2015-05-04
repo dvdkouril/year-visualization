750538da9763054f8fa601c2585aba827cd62856
David Kouřil <david.kouril@gmail.com>
2015-05-04 13:13:57 +0200

    Initial commit

---
+++ year-visualizationTests/year_visualizationTests.swift
@@ -0,0 +1,36 @@
+//
+//  year_visualizationTests.swift
+//  year-visualizationTests
+//
+//  Created by David Kouřil on 04/05/15.
+//  Copyright (c) 2015 dvdkouril. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class year_visualizationTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
